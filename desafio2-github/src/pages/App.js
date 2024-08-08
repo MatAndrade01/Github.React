40a491e586a8e474ae88fe38d16e8a82de924837
Matheus Andrade <ltbmatheus141012@gmail.com>
2024-08-08 16:34:30 -0300

    Criando botão buscar!!

--- desafio2-github/src/pages/App.js
+++ desafio2-github/src/pages/App.js
@@ -1,15 +1,23 @@
 
+import { useState } from 'react';
 import gitLogo from '../assets/github.png'
 import Input from '../components/input';
+import Button from '../components/Button';
 import ItemRepo from '../components/itemRepo';
 
 import { Container } from './style';
 
 function App() {
+
+  const [repos, setRepos] =  useState([]);
+
+
+
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt='github logo'/>
       <Input />
+      <Button />
       <ItemRepo />
     </Container>
   );
