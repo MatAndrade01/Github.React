95be2b02f68b1da8d28563814b896c71d6b70d3a
Matheus Andrade <ltbmatheus141012@gmail.com>
2024-08-08 14:55:22 -0300

    Criando componente de listagem de repositorio!!

--- desafio2-github/src/pages/App.js
+++ desafio2-github/src/pages/App.js
@@ -1,6 +1,7 @@
 
 import gitLogo from '../assets/github.png'
 import Input from '../components/input';
+import ItemRepo from '../components/itemRepo';
 
 import { Container } from './style';
 
@@ -9,6 +10,7 @@ function App() {
     <Container>
       <img src={gitLogo} width={72} height={72} alt='github logo'/>
       <Input />
+      <ItemRepo />
     </Container>
   );
 }
